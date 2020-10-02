42ed2ca603642b05b28df4dbeb68c1d4623300fe
igorg9810 <turgon98@gmail.com>
2020-10-02 16:28:10 +0300

    init: implement all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array === null || array.length == 0){
+    return 0;
+  }
+  array.sort(compareNumeric)
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array === null || array.length == 0){
+    return 0;
+  }
+  array.sort(compareNumeric)
+  return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array === null || array.length == 0){
+    return 0;
+  }
+  let sum = 0;
+  for (let elem of array){
+    sum += elem;
+  }
+  return sum/array.length;
 }
